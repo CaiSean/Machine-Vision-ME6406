5f15810f71780d0165a6c6826efbbac3a5fe729d
Sean Cai-Mac <xcai95@gmail.com>
2018-09-24 12:47:09 -0400

    Changes

--- HW2/Problem_2b.m
+++ HW2/Problem_2b.m
@@ -17,44 +17,38 @@ B = B{1};
 x0 = B(:, 2); 
 y0 = B(:, 1); 
 
-img_gau = imgaussfilt(img_gray, 3); 
-
-for i = 1:length(x0)-5
-    for j = 1:5
-        s_x(i, j) = x0(i+j); 
-        s_y(i, j) = y0(i+j); 
-    end
+for i = 1:(length(x0) - 1)
+    s(i, :) = sqrt((x0(i) - x0(i+1))^2 + (y0(i) - y0(i+1))^2); 
 end
 
-
+%%
 gaus_fun = @(s, sigma) 1/(sigma*sqrt(2*pi))*exp(-s^2/(2*sigma^2)); 
 
-for i = 1:length(s_x)
-    for j = 1:5
-        gaus_x = gaus_fun(s_x(i, j), 3); 
-        temp_X(j) = conv(s_x(i, j), gaus_x); 
-
-        gaus_y = gaus_fun(s_y(i, j), 3); 
-        temp_Y(j) = conv(s_y(i, j), gaus_y); 
-    end
-    X(i, :) = sum(temp_X); 
-    Y(i, :) = sum(temp_Y);
+for i = 1:length(s)
+    gaus(i, 1) = gaus_fun(s(i), 5); 
+    
+    X(i, 1) = conv(x0(i), gaus(i)); 
+    Y(i, 1) = conv(y0(i), gaus(i)); 
 end
 
 for i = 1:length(X)-1
-    X_dot(i, :) = X(i)-X(i+1); 
-    Y_dot(i, :) = Y(i)-Y(i+1); 
+    X_dot(i, :) = (X(i+1)-X(i))/(s(i+1)-s(i)); 
+    Y_dot(i, :) = (Y(i+1)-Y(i))/(s(i+1)-s(i)); 
 end
 
 for i = 1:length(X_dot)-1
-    X_ddot(i, :) = X_dot(i)-X_dot(i+1); 
-    Y_ddot(i, :) = X_dot(i)-X_dot(i+1); 
+    X_ddot(i, :) = (X_dot(i+1)-X_dot(i))/(s(i+1)-s(i)); 
+    Y_ddot(i, :) = (X_dot(i+1)-X_dot(i))/(s(i+1)-s(i)); 
 end
 
 for i = 1:length(X_ddot)
-    K(i, :) = X_dot(i)*Y_ddot(i)-Y_dot(i)*X_ddot(i); 
+    K(i, :) = X_dot(i)*Y_ddot(i)-Y_dot(i)*X_ddot(i)/...
+              (X_dot(i)^2+Y_dot(i)^2)^(3/2); 
 end
 
+
+%% Plot the diagram
+scatter(s(1:length(K)), K)
 
 
 
