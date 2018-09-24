77d50247e1b0239707425515e877d3252cbb8bc3
Sean Cai-Windows <xcai95@gmail.com>
2018-09-23 22:34:23 -0400

    Changes

--- HW2/Problem_2b.m
+++ HW2/Problem_2b.m
@@ -19,15 +19,59 @@ y0 = B(:, 1);
 
 img_gau = imgaussfilt(img_gray, 3); 
 
+for i = 1:length(x0)-5
+    for j = 1:5
+        s_x(i, j) = x0(i+j); 
+        s_y(i, j) = y0(i+j); 
+    end
+end
+
 
 gaus_fun = @(s, sigma) 1/(sigma*sqrt(2*pi))*exp(-s^2/(2*sigma^2)); 
 
-for i = 1:length(x0)
-    gaus_x(i, :) = gaus_fun(x0(i), 3); 
-    X(i, :) = conv(x0(i), gaus_x(i, :)); 
-    
-    gaus_y(i, :) = gaus_fun(y0(i), 3); 
-    Y(i, :) = conv(y0(i), gaus_y(i, :)); 
+for i = 1:length(s_x)
+    for j = 1:5
+        gaus_x = gaus_fun(s_x(i, j), 3); 
+        temp_X(j) = conv(s_x(i, j), gaus_x); 
+
+        gaus_y = gaus_fun(s_y(i, j), 3); 
+        temp_Y(j) = conv(s_y(i, j), gaus_y); 
+    end
+    X(i, :) = sum(temp_X); 
+    Y(i, :) = sum(temp_Y);
 end
+
+for i = 1:length(X)-1
+    X_dot(i, :) = X(i)-X(i+1); 
+    Y_dot(i, :) = Y(i)-Y(i+1); 
+end
+
+for i = 1:length(X_dot)-1
+    X_ddot(i, :) = X_dot(i)-X_dot(i+1); 
+    Y_ddot(i, :) = X_dot(i)-X_dot(i+1); 
+end
+
+for i = 1:length(X_ddot)
+    K(i, :) = X_dot(i)*Y_ddot(i)-Y_dot(i)*X_ddot(i); 
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
