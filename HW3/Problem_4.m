8b706015d02d3a165332d778926cf841ae515a15
Sean Cai-Windows <xcai95@gmail.com>
2018-10-28 23:37:30 -0400

    Changes

--- HW3/Problem_4.m
+++ HW3/Problem_4.m
@@ -10,21 +10,24 @@ clear all;
 img = imread('chip.jpg');
 
 %% Image Process
-SE_lead = ones(5, 11);
-SE_chip = ones(147, 82);
+img_BG = zeros(184, 273); 
+
+SE_lead = strel('rectangle', [5 11]);
+SE_chip = strel('rectangle', [147 82]);
+SE_1 = strel('rectangle', [8 16]);
+SE_2 = strel('rectangle', [35 23]);
 
 % Path 1
 img_BW_1 = im2bw(img, 0.36); 
 img_BW_1 = imcomplement(img_BW_1);
-img_morph_1 = imdilate(img_BW_1, SE_lead);
+img_morph_1 = imdilate(img_BW_1, SE_1);
 img_morph_1 = imcomplement(img_morph_1); 
-img_morph_1 = imerode(img_morph_1, SE_lead);
-% img_morph_1 = imcomplement(img_morph_1); 
-% img_morph_1 = imerode(img_morph_1, SE_chip);
-% img_morph_1 = imerode(img_morph_1, SE_lead);
-% img_morph_1 = imerode(img_morph_1, SE_lead);
 imshow(img_morph_1)
-img_morph_1 = bwmorph(img_morph_1, 'shrink');
+img_morph_1 = imerode(img_morph_1, SE_2);
+
+img_morph_1 = bwmorph(img_morph_1, 'shrink', inf);
+
+
 
 img_morph_1 = imdilate(img_morph_1, SE_lead);
 
