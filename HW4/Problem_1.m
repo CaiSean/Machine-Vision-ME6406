cd0a673751cc04e40f6292d630f4d8ca41bb700e
Sean Cai-Mac <xcai95@gmail.com>
2018-11-09 13:11:22 -0500

    changes

---
+++ HW4/Problem_1.m
@@ -0,0 +1,14 @@
+%% Machine Vision Homework 4
+% * Problem 1
+% * Author: Xinyi Cai
+
+%% Housekeeping
+clear all; clc
+
+%% Parameters
+H_out = [1 0 0 0]; 
+E_out = [0 1 0 0]; 
+L_out = [0 0 1 0]; 
+O_out = [0 0 0 1]; 
+
+Np = 4; Nj = 15; Nk = 25; 
--- HW4/Training_data.m
+++ HW4/Training_data.m
@@ -86,5 +86,33 @@ O = [0 1 1 1 0;
 
 training_data_3 = {H, E, L, O};
 
+%% Test Data Set 1
+H = [1 0 0 0 1; 
+     1 0 0 0 1; 
+     1 1 1 1 1; 
+     1 0 0 0 1; 
+     1 0 0 0 0]; 
+
+E = [1 1 1 1 1; 
+     1 0 0 0 0; 
+     1 1 1 1 1; 
+     1 0 0 0 0; 
+     1 1 1 1 1]; 
+
+L = [1 0 0 0 0; 
+     1 0 0 0 0; 
+     1 0 0 0 0; 
+     1 0 0 0 0; 
+     1 1 1 0 0]; 
+
+O = [0 1 1 1 0; 
+     1 0 0 0 1; 
+     1 0 0 0 1; 
+     1 0 0 0 1; 
+     0 1 1 1 0]; 
+
+testing_data = {H, E, L, O};
+
 %% Save data
 save training_data.mat training_data_1 training_data_2 training_data_3
+save testing_data.mat testing_data
