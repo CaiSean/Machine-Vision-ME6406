3a0dc389837b3e8361b9e18d9311a6fe102a594d
Sean Cai-Mac <xcai95@gmail.com>
2018-11-12 17:16:52 -0500

    Changes

--- HW4/Problem_1_testing.m
+++ HW4/Problem_1_testing.m
@@ -14,7 +14,6 @@ H_testing = testing_data{1, 1};
 E_testing = testing_data{1, 2}; 
 L_testing = testing_data{1, 3}; 
 O_testing = testing_data{1, 4}; 
-
 %% Calculation
 [y_P_H_test, y_Q_H_test] = Forward_Phase(H_testing, weight_pj_H, weight_qp_H); 
 [y_P_E_test, y_Q_E_test] = Forward_Phase(E_testing, weight_pj_E, weight_qp_E); 
